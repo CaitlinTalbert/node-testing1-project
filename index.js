d6a04c5ce9afdb76c76a7da225006ade7c4de2a6
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-27 16:02:52 -0500

    Exercise 5

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.seasons = ["summer", "fall", "winter", "spring"];
+    this.currentSeason = 0;
   }
 
   /**
@@ -96,7 +97,9 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    let result = this.seasons[this.currentSeason];
+    this.currentSeason === 3 ? (this.currentSeason = 0) : this.currentSeason++;
+    return result;
   }
 }
 
